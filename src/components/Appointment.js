97f6acfb4b61c99c938cfb1c9c50f2fbc58d5144
anilgardd <98427173+anilgardd@users.noreply.github.com>
2023-05-20 00:07:59 +0300

    initial commit

--- src/components/Appointment.js
+++ src/components/Appointment.js
@@ -1,33 +1,37 @@
-import React from 'react';
+import React from "react";
 
 const Appointment = () => {
   return (
-    <div className='bg-accent w-full p-8 rounded-sm max-w-[22rem] md:max-w-md mx-auto'>
-      <h3 className='text-white text-[28px] font-bold tracking-[0.3px] text-center'>
-        Get an appointment
+    <div className="bg-black w-full p-8 rounded-sm max-w-[22rem] md:max-w-md mx-auto">
+      <h3 className="text-white text-[28px] font-bold tracking-[0.3px] text-center">
+        Başvuru Oluşturun
       </h3>
       {/* bar */}
-      <div className='w-[54px] h-[3px] bg-white my-6 mx-auto'></div>
+      <div className="w-[54px] h-[3px] bg-white my-6 mx-auto"></div>
       {/* input group */}
-      <div className='space-y-[24px]'>
-        <input className='form-control' placeholder='Full name' type='text' />
+      <div className="space-y-[24px]">
         <input
-          className='form-control'
-          placeholder='Phone number'
-          type='text'
+          className="form-control"
+          placeholder="Adınız Soyadınız"
+          type="text"
         />
         <input
-          className='form-control'
-          placeholder='Email address'
-          type='email'
+          className="form-control"
+          placeholder="Telefon Numaranız"
+          type="text"
+        />
+        <input
+          className="form-control"
+          placeholder="Email Adresiniz"
+          type="email"
         />
         <textarea
-          className='resize-none w-full h-[132px] outline-none rounded-sm p-4 font-body text-sm text-gray focus:ring-1 focus:ring-primary'
-          placeholder='Your message'
+          className="resize-none w-full h-[132px] outline-none rounded-sm p-4 font-body text-sm text-gray focus:ring-1 focus:ring-primary"
+          placeholder="Mesajınızı Yazınız"
         ></textarea>
         {/* Button */}
-        <button className='btn bg-primary hover:bg-primary-hover transition-all'>
-          Send message
+        <button className="btn bg-primary hover:bg-primary-hover transition-all">
+          Başvuru Oluştur
         </button>
       </div>
     </div>
--- src/components/Subscribe.js
+++ src/components/Subscribe.js
@@ -1,22 +1,20 @@
-import React from 'react';
+import React from "react";
 
 const Subscribe = () => {
   return (
-    <section className='bg-accent pt-[54px] pb-[74px] min-h-[300px]'>
-      <div className='container mx-auto px-8 text-center'>
-        <h3 className='font-primary font-extrabold leading-[1.2] text-white text-[40px] mb-[12px]'>
+    <section className="bg-accent pt-[54px] pb-[74px] min-h-[300px]">
+      <div className="container mx-auto px-8 text-center">
+        <h3 className="font-primary font-extrabold leading-[1.2] text-white text-[40px] mb-[12px]">
           Subscribe to our newsletter
         </h3>
-        <p className='mb-[40px] text-white text-sm'>
-          Be the first to get the latest news about us.
-        </p>
-        <form className='max-w-[600px] mx-auto flex flex-col lg:flex-row'>
+        <p className="mb-[40px] text-white text-sm">İlk duyan siz olun.</p>
+        <form className="max-w-[600px] mx-auto flex flex-col lg:flex-row">
           <input
-            className='form-control mb-4'
-            type='text'
-            placeholder='Your email address'
+            className="form-control mb-4"
+            type="text"
+            placeholder="Your email address"
           />
-          <button className='btn bg-primary hover:bg-primary-hover lg:max-w-[150px] lg:ml-4'>
+          <button className="btn bg-primary hover:bg-primary-hover lg:max-w-[150px] lg:ml-4">
             Join
           </button>
         </form>
